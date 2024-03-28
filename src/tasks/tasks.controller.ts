1b981217b966d64193cb41b6127833cf1951c162
Randolph Raymond Carmona <raymondcarmonatejeda01@gmail.com>
2024-03-28 11:52:37 -0400

    refactoring repository

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,43 +1,53 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query } from '@nestjs/common';
 import { TasksService } from './tasks.service';
-import { Task, TaskStatus } from './task.model';
+import { TaskStatus } from './task-status.enum';
 import { title } from 'process';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
+import { Task } from './task.entity';
 
 
 @Controller('tasks')
 export class TasksController {
     constructor(private tasksService: TasksService) { }
 
-    @Get()
-    getTask(@Query() filterDto: GetTasksFilterDto): Task[] {
-        if(Object.keys(filterDto).length) {
-           return this.tasksService.getTasksWithFilters(filterDto);
-        }else{
-            return this.tasksService.getAllTasks();  
-        }
-    }
+
+
+
+    // @Get()
+    // getTask(@Query() filterDto: GetTasksFilterDto): Task[] {
+    //     if(Object.keys(filterDto).length) {
+    //        return this.tasksService.getTasksWithFilters(filterDto);
+    //     }else{
+    //         return this.tasksService.getAllTasks();  
+    //     }
+    // }
+
+    // @Get('/:id')
+    // getTaskById(@Param('id') id: string) Promise<Task> {
+    //     const Myid = id;
+    //     return this.tasksService.getTasksById(id);
+    // }
     
     @Get('/:id')
-    getTaskById(@Param('id') id: string): Task {
+    getTaskById(@Param('id') id: string): Promise<Task> {
         return this.tasksService.getTaskById(id);
     }
 
-    @Post()
-    createTask(@Body() createTaskDto: CreateTaskDto): Task {
-        return this.tasksService.createTask(createTaskDto);
-    }
+    // @Post()
+    // createTask(@Body() createTaskDto: CreateTaskDto): Task {
+    //     return this.tasksService.createTask(createTaskDto);
+    // }
 
-    @Patch('/:id/status')
-    updateTaskStatus(@Param('id') id: string, @Body() updateTaskStatusDto: UpdateTaskStatusDto): Task {
-        const { status } = updateTaskStatusDto;
-        return this.tasksService.updateTaskStatus(id, status);
-    }
+    // @Patch('/:id/status')
+    // updateTaskStatus(@Param('id') id: string, @Body() updateTaskStatusDto: UpdateTaskStatusDto): Task {
+    //     const { status } = updateTaskStatusDto;
+    //     return this.tasksService.updateTaskStatus(id, status);
+    // }
 
-    @Delete('/:id')
-    deleteTask(@Param('id') id: string): void {
-        this.tasksService.deleteTask(id);
-    }
+    // @Delete('/:id')
+    // deleteTask(@Param('id') id: string): void {
+    //     this.tasksService.deleteTask(id);
+    // }
 }
--- src/tasks/tasks.repository.ts
+++
@@ -1,8 +0,0 @@
-import { Repository } from "typeorm";
-import { GetTasksFilterDto } from "./dto/get-tasks-filter.dto";
-import { Task, TaskStatus } from "./task.model";
-import { CreateTaskDto } from "./dto/create-task.dto";
-
-export class TaskRepository extends Repository<Task> {
-
-}
