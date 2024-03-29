73a64a3355b4288ae6e843243cae8104842b19a1
Randolph Raymond Carmona <raymondcarmonatejeda01@gmail.com>
2024-03-29 00:44:05 -0400

    Refactor TaskCustomRepository to extend Repository class

--- src/tasks/TaskRepository/task.repository.ts
+++ src/tasks/TaskRepository/task.repository.ts
@@ -1,34 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { Task } from '../task.entity';
 import { ITaskRepository } from './task.repository.interface';
 import { CreateTaskDto } from '../dto/create-task.dto';
 import { TaskStatus } from '../task-status.enum';
 
 @Injectable()
-export class TaskCustomRepository implements ITaskRepository {
-    constructor(
-        @InjectRepository(Task)
-        private readonly taskRepository: Repository<Task>,
-    ) { }
+export class TaskCustomRepository extends Repository<Task> implements ITaskRepository {
+    constructor(private dataSource: DataSource) {
+        super(Task, dataSource.createEntityManager());
+    }
     getTaskById(id: string): Promise<Task> {
         throw new Error('Method not implemented.');
     }
     async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
-        const task = this.taskRepository.create({ ...createTaskDto, status: TaskStatus.OPEN });
-        return await this.taskRepository.save(task);
+        const task = this.create({ ...createTaskDto, status: TaskStatus.OPEN });
+        return await this.save(task);
     }
     async deleteTask(id: string): Promise<void> {
-        return await this.taskRepository.delete(id).then(() => { });
+        return await this.delete(id).then(() => { });
     }
     async updateTaskStatus(id: string, status: TaskStatus): Promise<Task> {
         const task = await this.getTaskById(id);
         task.status = status;
-        return await this.taskRepository.save(task);
+        return await this.save(task);
     }
     getTasks(): Promise<Task[]> {
-        return this.taskRepository.find();
+        return this.find();
     }
 
     // Aquí puedes agregar más métodos personalizados
